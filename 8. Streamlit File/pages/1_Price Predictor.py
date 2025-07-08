ab34d48235b62b65bdea9d7da32238633a89c054
Aftab Alam <96426192+aftabalam1210@users.noreply.github.com>
2025-07-08 18:51:47 +0000

    "pickle changes"

--- 8. Streamlit File/pages/1_Price Predictor.py
+++ 8. Streamlit File/pages/1_Price Predictor.py
@@ -7,11 +7,16 @@ st.set_page_config(page_title="Viz Demo")
 
 
 
-with open('df.pkl','rb') as file:
+with open('datasets/df.pkl', 'rb') as file:
     df = pickle.load(file)
+# with open('df.pkl','rb') as file:
 
-with open('pipeline.pkl','rb') as file:
+#     df = pickle.load(file)
+
+with open('datasets/pipeline.pkl', 'rb') as file:
     pipeline = pickle.load(file)
+# with open('pipeline.pkl','rb') as file:
+#     pipeline = pickle.load(file)
 
 
 st.header('Enter your inputs')
